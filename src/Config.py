d60c9f2cd4c9654da78b5dd26ad72d7843eea136
Dmitry V <dimonvechorko@gmail.com>
2024-12-09 19:32:54 +0300

    Adding Constants

---
+++ src/Config.py
@@ -0,0 +1,4 @@
+class Config:
+    """Конфигурация для входных и выходных директорий"""
+    INPUT_DIR = "D:/data2sc-parser/input_json"  # Путь к входной директории
+    OUTPUT_DIR = "D:/data2sc-parser/output_scs"  # Путь к выходной директории
